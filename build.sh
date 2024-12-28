6c677232080e30290c7ef5309ec0f9da95317f4a
Dmitry Mikhin <dmikhin@webmonitorx.ru>
2024-12-28 18:13:08 +0300

    removed unused

--- build.sh
+++
@@ -1,4 +0,0 @@
-set -ex
-
-docker build --no-cache --pull \
- --tag wmx-public.gitlab.yandexcloud.net:5050/wmx-public/container-images/meganode:latest .
--- build46.sh
+++
@@ -1,5 +0,0 @@
-set -ex
-
-docker build --no-cache --pull \
- --tag wmx-public.gitlab.yandexcloud.net:5050/wmx-public/container-images/meganode:4.6.48-1 .
-docker push wmx-public.gitlab.yandexcloud.net:5050/wmx-public/container-images/meganode:4.6.48-1
