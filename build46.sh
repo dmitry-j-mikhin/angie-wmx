fb1b6952b5c2d37baec14b9d8b95e4a4d8d37677
Dmitry Mikhin <dmikhin@webmonitorx.ru>
2024-12-28 17:47:00 +0300

    4.6 last

--- build46.sh
+++ build46.sh
@@ -1,5 +1,5 @@
 set -ex
 
 docker build --no-cache --pull \
- --tag wmx-public.gitlab.yandexcloud.net:5050/wmx-public/container-images/meganode:4.6.48-1 .
-docker push wmx-public.gitlab.yandexcloud.net:5050/wmx-public/container-images/meganode:4.6.48-1
+ --tag wmx-public.gitlab.yandexcloud.net:5050/wmx-public/container-images/meganode:4.6.49-1 .
+docker push wmx-public.gitlab.yandexcloud.net:5050/wmx-public/container-images/meganode:4.6.49-1
